f55d61ebf02f2b0c9e1e0ab96ef11361b17995e7
Jazzmin <jazz@Jazzmins-MacBook.local>
2016-11-17 00:55:19 +0200

    text support

---
+++ src/services/http.service.ts
@@ -0,0 +1,51 @@
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+
+
+@Injectable()
+export class HttpService {
+
+constructor (private http : Http ) {
+}
+
+  public getJson() {
+ // var url = '../model/data.json';
+  var url = 'http://echo.jsontest.com/key/value/one/two';
+  var url = 'http://date.jsontest.com/';
+  return this.http.get(url).map(res  => res.json());
+  }
+
+  public getText() {
+  var urltext ='../model/data.txt'; 
+  return this.http.get(urltext).map(res  => res.text());
+  }
+
+
+  public postJson() {
+    var json = JSON.stringify({var1: 'test',
+  var2: 3, var3 : 'bla'});
+  var params = 'json=' + json;
+  var headers = new Headers();
+  headers.append('Content-Type',
+  'application/x-www-form-urlencoded' );
+  return this.http.post('http://validate.jsontest.com', params, {
+    headers: headers
+  }).map (res=> res.json());
+
+  }
+
+
+
+
+/////
+public getJsonFromServer() : Observable<any>{
+  var url = '../model/pending.json';
+  var urltext ='../model/text.txt'; 
+  return this.http.get(url).map(res  => res.json());
+
+}
+
+}
